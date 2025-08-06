7259d9ceef96a358bcfb0c0240b05664e3347022
lmecheverria2 <116749503+lmecheverria2@users.noreply.github.com>
2025-08-06 18:08:45 -0500

    Colas corregidas

--- agricultor/app/main.py
+++ agricultor/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routes import agricultor, cosecha
 from app.database import Base, engine
+from app.rabbit import setup_colas
+
+setup_colas()
+
 
 Base.metadata.create_all(bind=engine)
 
